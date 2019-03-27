90b46a1675e3b88bb6d21644970e85c8b81f4566
Paksawat <paksawat@gmail.com>
2019-03-27 20:00:48 +0100

    fixes og popup menu
    små justeringer i feed
    tilføjet like counter - skal kigges på
    lavet pop up menu + click events

--- jokeOff comp/script/script.js
+++ jokeOff comp/script/script.js
@@ -19,25 +19,7 @@ for (i = 0; i < allMenu2Links.length; i++) {
 }
 
 
-// tager fat i like ikonet
-let likeIcon = document.querySelector('.jokeContainer__like'); 
-
-//click event på like ikonet
-likeIcon.addEventListener('click', function(){
-	// tager fat i billedestigen i like ikonet
-	let likeIconSrc = likeIcon.getAttribute("src")
-	//Hvis billedestigen = ikke farvet. sæt den til farvet
-	if(likeIconSrc === "img/heart_line.svg"){
-		likeIcon.src = "img/heart_colored.svg";
-	}else{ // ellers sæt den til ikke farvet
-		likeIcon.src = "img/heart_line.svg";
-	}
-});
-
-
-
-
-
+// Jarne sessionStorage gemt i array
 
 let jokeSubmit = document.querySelector(".jokeInput");
 let jokeInput = document.querySelector(".writeJoke");
@@ -72,33 +54,123 @@ jokeSubmit.addEventListener("click", function (e) {
 });
 
 function showJokes() {
-       // console.log("Showing the jokes");
-        // reset the display of jokes
-        showJoke.innerHTML = "";
+   // console.log("Showing the jokes");
+    // reset the display of jokes
+    showJoke.innerHTML = "";
 
-        // Display the jokes
-        for(let i=0;i<storedJokes.length;i++) {
-            // create a new article tag
-            let article = document.createElement("article");
-            article.classList.add("jokeContainer");
-            
-            // Set the content of the article
-            article.innerHTML = 
-               '<header class="jokeContainer__header">'
-			+'<img class="jokeContainer__avatar" src="img/avatar.svg" alt="">'
-			+'<h3 class="jokeContainer__username">JokeMaster69</h3>'
-			+'<img class="jokeContainer__crown" src="img/crown.svg" alt="">'
-			+'</header>'
-			+'<pre class="jokeContainer__joke">'
-			+ storedJokes[i].joke
-			+'</pre>'
-			+'<div class="jokeContainer__footer">'
-			+'<img class="jokeContainer__like" src="img/heart_line.svg" alt="">'
-			+'<img class="jokeContainer__menu" src="img/pop_menu.svg" alt="">'
-			+'</div>'
-			;
-            //console.log(article);
-            showJoke.appendChild(article);
-        }
+    // Display the jokes
+    for(let i=0;i<storedJokes.length;i++) {
+        // create a new article tag
+        let article = document.createElement("article");
+        article.classList.add("jokeContainer");
+        
+        // Set the content of the article
+        article.innerHTML = 
+           '<header class="jokeContainer__header">'
+		+'<img class="jokeContainer__avatar" src="img/avatar.svg" alt="">'
+		+'<h3 class="jokeContainer__username">JokeMaster69</h3>'
+		+'<img class="jokeContainer__crown" src="img/crown.svg" alt="">'
+		+'</header>'
+		+'<pre class="jokeContainer__joke">'
+		+ storedJokes[i].joke
+		+'</pre>'
+		+'<div class="jokeContainer__footer">'
+		+'<img class="jokeContainer__like" src="img/heart_line.svg" alt="">'
+		+'<p class="likesNumber"></p>'
+		+'<img class="jokeContainer__menu" src="img/pop_menu.svg" alt="">'
+		+'</div>'
+		;
+        //console.log(article);
+        showJoke.appendChild(article);
     }
+}
 
+
+
+
+
+
+// tager fat i like ikonet
+let likeIcon = document.querySelector('.jokeContainer__like'); 
+
+//click event på like ikonet
+likeIcon.addEventListener('click', function(){
+	// tager fat i billedestigen i like ikonet
+	let likeIconSrc = likeIcon.getAttribute("src")
+	//Hvis billedestigen = ikke farvet. sæt den til farvet
+	if(likeIconSrc === "img/heart_line.svg"){
+		likeIcon.src = "img/heart_colored.svg";
+	}else{ // ellers sæt den til ikke farvet
+		likeIcon.src = "img/heart_line.svg";
+	}
+});
+
+
+
+
+
+
+
+
+
+let splash = document.querySelector(".splashScreen");
+
+// fra https://stackoverflow.com/questions/6121203/how-to-do-fade-in-and-fade-out-with-javascript-and-css
+function splashTimeOut(){
+		var op = 1;  // initial opacity
+	    var timer = setInterval(function () {
+	        if (op <= 0.1){
+	            clearInterval(timer);
+	            splash.style.display = 'none';
+	        }
+	        splash.style.opacity = op;
+	        splash.style.filter = 'alpha(opacity=' + op * 100 + ")";
+	        op -= op * 0.1;
+	    }, 20);
+}
+// caller timeout funktion med tid
+setTimeout(splashTimeOut, 10);
+
+
+// like random number + counter
+
+let like = document.querySelector(".likesNumber");
+let heart = document.querySelector(".jokeContainer__like");
+
+//random number generator 1 - 100
+var x = Math.floor((Math.random() * 100) + 1);
+// sætter like til at være vores random nummer
+like.innerHTML = x;
+
+heart.addEventListener("click", function() {
+	// tager fat i billedestigen i like ikonet
+	let likeIconSrc = likeIcon.getAttribute("src")
+	//Hvis billedestigen = ikke farvet. like +1
+	if(likeIconSrc === "img/heart_line.svg"){
+		console.log("true");
+		like.innerHTML = x;
+		x++;
+	}else{ // ellers like -1
+		like.innerHTML = x;
+		x--;
+		console.log("false");
+	}
+
+});
+
+// Feed pop menu
+
+let popUpIcon = document.querySelector(".jokeContainer__menu");
+let popUpMenu = document.querySelector(".jokeContainer__popMenu");
+
+// click event på menu ikon
+popUpIcon.addEventListener("click", function(){
+	//sætter menu til at vises
+	popUpMenu.style.display = "block";
+});
+
+// click event på hele pop up ul
+popUpMenu.addEventListener("click", function(){
+	//sætter menu til ikke at vises
+	popUpMenu.style.display = "none";
+});
