a971ec7a5924ecd0ba1cef921ffc3b0323cf152f
Paksawat <paksawat@gmail.com>
2019-03-27 16:10:42 +0100

    show joke
    Opdateret js på show joke, til jarnes forslag.
    
    Ændret joke feed bars til grid setup

--- jokeOff comp/script/script.js
+++ jokeOff comp/script/script.js
@@ -19,14 +19,17 @@ for (i = 0; i < allMenu2Links.length; i++) {
 }
 
 
-
+// tager fat i like ikonet
 let likeIcon = document.querySelector('.jokeContainer__like'); 
 
+//click event på like ikonet
 likeIcon.addEventListener('click', function(){
+	// tager fat i billedestigen i like ikonet
 	let likeIconSrc = likeIcon.getAttribute("src")
+	//Hvis billedestigen = ikke farvet. sæt den til farvet
 	if(likeIconSrc === "img/heart_line.svg"){
 		likeIcon.src = "img/heart_colored.svg";
-	}else{
+	}else{ // ellers sæt den til ikke farvet
 		likeIcon.src = "img/heart_line.svg";
 	}
 });
@@ -34,14 +37,68 @@ likeIcon.addEventListener('click', function(){
 
 
 
-// SessionStorage til at oprette en ny joke
+
 
 let jokeSubmit = document.querySelector(".jokeInput");
 let jokeInput = document.querySelector(".writeJoke");
+let showJoke = document.querySelector(".showJoke");
 
-jokeSubmit.addEventListener('click', function(){
-	var jokeText = jokeInput.value;
-	sessionStorage.setItem("joke", jokeText);
-	console.log(sessionStorage);
+
+// tomt array til at gemme jokes
+let storedJokes = [];
+// if jokes are stored in session storage, load them from there...
+if(sessionStorage.getItem("jokes")) {
+    storedJokes = JSON.parse(sessionStorage.getItem("jokes"));
+    showJokes();
+}
+
+// handle submit
+jokeSubmit.addEventListener("click", function (e) {
+    // create new joke object
+
+    let tempObject = {
+        joke: jokeInput.value,
+        };
+    console.log(tempObject);
+
+    // add to array of jokes
+    storedJokes.push(tempObject);
+    console.log(storedJokes);
+
+    // save to session storage
+    sessionStorage.setItem("jokes", JSON.stringify(storedJokes));
+    showJokes();
+
 });
-console.log(sessionStorage);
+
+function showJokes() {
+       // console.log("Showing the jokes");
+        // reset the display of jokes
+        showJoke.innerHTML = "";
+
+        // Display the jokes
+        for(let i=0;i<storedJokes.length;i++) {
+            // create a new article tag
+            let article = document.createElement("article");
+            article.classList.add("jokeContainer");
+            
+            // Set the content of the article
+            article.innerHTML = 
+               '<header class="jokeContainer__header">'
+			+'<img class="jokeContainer__avatar" src="img/avatar.svg" alt="">'
+			+'<h3 class="jokeContainer__username">JokeMaster69</h3>'
+			+'<img class="jokeContainer__crown" src="img/crown.svg" alt="">'
+			+'</header>'
+			+'<pre class="jokeContainer__joke">'
+			+ storedJokes[i].joke
+			+'</pre>'
+			+'<div class="jokeContainer__footer">'
+			+'<img class="jokeContainer__like" src="img/heart_line.svg" alt="">'
+			+'<img class="jokeContainer__menu" src="img/pop_menu.svg" alt="">'
+			+'</div>'
+			;
+            //console.log(article);
+            showJoke.appendChild(article);
+        }
+    }
+
